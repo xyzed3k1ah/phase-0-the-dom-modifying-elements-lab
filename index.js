2c94f297035cff0b51dab4e2f30ebafed093aef7
Zedikiah <xyzed3k1ah@gmail.com>
2023-07-24 22:36:32 -0600

    dom modifying element lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Zedikiah is the champion";
+
+console.log(h1.textContent);
+
